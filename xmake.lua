3e6243ec4e0c3622baf086a11f7db45107a9a3b0
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-08-23 16:12:01 +0600

    Switch from premake5 to xmake

---
+++ xmake.lua
@@ -0,0 +1,10 @@
+add_requires('openssl3')
+target("kcflib")
+	set_kind("static")
+	add_packages("openssl3")
+	add_files("src/kcflib/*.c")
+	add_headerfiles("include/(kcflib/*.h)")
+	add_includedirs("include/kcflib", {public = true})
+	add_rules("utils.install.cmake_importfiles")
+	add_rules("utils.install.pkgconfig_importfiles")
+
