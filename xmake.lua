3d453e207df84c1749f03297a4d18619c6ee1ba5
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-08-26 07:47:38 +0000

    kcf: using io as package instead of OpenSSL BIO

--- xmake.lua
+++ xmake.lua
@@ -1,20 +1,12 @@
--- TODO replace with my IO
-add_requires('openssl3')
-
 add_defines("_FILE_OFFSET_BITS=64")
-
-target("io")
-	set_kind("static")
-	add_files("src/io/*.c")
-	add_headerfiles("include/(io/*.h)")
-	add_includedirs("include", {public = true})
 
 target("kcflib")
 	set_kind("static")
-	add_packages("openssl3")
+	add_packages("io")
 	add_files("src/kcflib/*.c")
 	add_headerfiles("include/(kcf/*.h)")
 	add_includedirs("include", {public = true})
+	add_includedirs("io/include")
 	add_rules("utils.install.cmake_importfiles")
 	add_rules("utils.install.pkgconfig_importfiles")
 
